23b5a1fc7c65f92ae918f88805334a088a4d4cc4
matt-izatt <matt.izatt@hotmail.co.uk>
2020-08-04 21:27:19 +0100

    Added comments to router outlet parent component

--- src/app/router-outlet/single/components/parent/parent.component.ts
+++ src/app/router-outlet/single/components/parent/parent.component.ts
@@ -7,12 +7,17 @@ import {Component} from '@angular/core';
 })
 export class ParentComponent {
 
+  // A variable which will store a reference to any type of component
   childComponentRef: any;
 
+  // Method gets called when the router loads the child component into the router outlet
   setComponentReference(component: any) {
+
+    // Stores a reference to the child component
     this.childComponentRef = component;
   }
 
+  // Calls method on the child component using the reference
   logFromChildComponent() {
     this.childComponentRef.logToConsole();
   }
